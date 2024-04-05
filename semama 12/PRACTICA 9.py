05cae4a7c2eb71eaa31878f23ebafeb1734a2521
RicardoRivas117 <157128299+RicardoRivas117@users.noreply.github.com>
2024-04-05 08:06:44 -0600

    Add files via upload

---
+++ semama 12/PRACTICA 9.py
@@ -0,0 +1,54 @@
+# print ("IF")
+#     # Pedir su edad
+# edad = int(input("Por favor, ingresa tu edad: "))
+
+# # si la edad es mayor a 18 años
+# if edad >= 18:
+#     print("Debes tramitar tu DPI")
+# else:
+#     print("Eres menor de edad.")
+
+
+
+# print ("if")
+#     # Solicitar al usuario que ingrese un número
+# numero = float(input("Por favor, ingresa un número: "))
+
+#     # Verificar si el número es positivo, negativo o cero
+# if numero >= 0:
+#         print("El número ingresado es positivo.")
+# elif numero < 0:
+#         print("El número ingresado es negativo.")
+# else:
+#         print("El número ingresado es cero.")
+
+
+
+# # Solicitar al usuario que ingrese un número
+# numero = int(input("Por favor, ingresa un número: "))
+
+# # la estructura switch 
+# numeros_letras = {
+#     1: "uno",
+#     2: "dos",
+#     3: "tres",
+#     4: "cuatro",
+#     5: "cinco"
+# }
+
+# # Verificar si el número está en el rango de 1 a 5 e imprimir su equivalente en letras
+# if numero in numeros_letras:
+#     print("El número ingresado es:", numeros_letras[numero])
+# else:
+#     print("Número no definido.")
+
+
+
+# # Inicializamos el contador
+# i = 1
+
+# # Imprimimos la tabla de multiplicar del número 2 utilizando un bucle while
+# while i <= 10:
+#     resultado = 2 * i
+#     print("2 *", i, "=", resultado)
+#     i += 1  # Incrementamos el contador en cada iteración
